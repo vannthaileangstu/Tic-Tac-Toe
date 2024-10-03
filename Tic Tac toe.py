52fc48f88ca5596aa6bd29636b8bd021116bde35
vanthaileangstu <vanthai.leang.stu@cedarpark.org>
2024-10-03 10:56:46 -0700

    print basic_board

---
+++ Tic Tac toe.py
@@ -0,0 +1,23 @@
+board = [['.','.','.'],
+         ['.','.','.'],
+         ['.','.','.']]
+
+def print_board():
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+def play():
+    pass
+
+
+#main program ------
+#print_instruction()
+
+print_board()
+#If no winner than repeat
+print_board()
+play()
+#check_winner()
+
+
