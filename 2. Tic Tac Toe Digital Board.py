74bf59aeaa8d9db5fe900c7a53241fb6c1e12387
vanthaileangstu <vanthai.leang.stu@cedarpark.org>
2024-11-04 10:51:01 -0800

    Final

--- 2. Tic Tac Toe Digital Board.py
+++ 2. Tic Tac Toe Digital Board.py
@@ -1,6 +1,6 @@
-import tkinter
+from tkinter import *
+import random
 
-current_player = "X"
 Grey = "#0F0F0F"
 Black = "#000000"
 White = "#ffffff"
@@ -9,35 +9,94 @@ Green = "#008000"
 Blue = "#0000FF"
 Yellow = "#FFFF00"
 
-board = [[0,0,0],
-         [0,0,0], 
-         [0,0,0]]
+window = Tk()
+window.title("Vannthai's Tic-Tac-Toe")
+players = ["x","o"]
+player = random.choice(players)
+buttons = [[0,0,0],
+           [0,0,0],
+           [0,0,0]]
 
-def restart_game():
-    print("RESTART")
 
-def tile_click(r, c):
-    print(r, c)
+label = Label(text=player + " turn", font=('consolas',30))
+label.pack(side="top")
 
-window = tkinter.Tk()
-window.title("Tic Tac Toe Game")
-window.resizable(False, False)
 
-frame = tkinter.Frame(window, width=600, height= 600)
-frame.grid_propagate(True)
+def new_game():
+    global player
+    player = random.choice(players)
+    label.config(text=player+" turn")
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="",bg="red")
+
+reset_button = Button(text="Restart", font=('consolas',25), command=new_game)
+reset_button.pack(side="bottom")
+
+frame = Frame(window, width=600, height= 600)
 frame.pack()
 
-message = tkinter.Label(frame, text = current_player+ "'s turn", font=("arial", 30))
-message.grid(row=0, column=1)
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(frame, text="",font=('consolas',40), width=5, height=2,
+                                      bg="blue" ,command= lambda row=row, column=column: player_turn(row,column))
+        buttons[row][column].grid(row=row,column=column)
 
-for  row in range(3):
-    for col in range(3):
-        board[row][col] = tkinter.Button(frame, text="", font=("arial",50,"bold"), bg=Yellow, fg= Black,
-                                         width=4, height=1, name=str(row)+","+str(col), command=lambda r=row,c=col: tile_click(r,c))
-        board[row][col].grid(row=row+1, column=col)
+def player_turn(row, column):
+    global player
+    if buttons[row][column]['text'] == "" and check_winer() is False:
+        if player == players[0]:
+            buttons[row][column]['text'] = player
+            if check_winer() is False:
+                player = players[1]
+                label.config(text=(players[1]+" turn"))
+            elif check_winer() is True:
+                label.config(text=(players[0]+" is the winner"))
+            elif check_winer() == "Draw":
+                label.config(text="Draw!")
+        else:
+            buttons[row][column]['text'] = player
+            if check_winer() is False:
+                player = players[0]
+                label.config(text=(players[0]+" turn"))
+            elif check_winer() is True:
+                label.config(text=(players[1]+" is the winner"))
+            elif check_winer() == "Draw":
+                label.config(text="Draw!")
 
-restart = tkinter.Button(frame, text = "Restart", font=("arial", 30, "bold"), command=lambda:restart_game())
-restart.grid(row=4,column=1)
 
-window.update()
+
+def check_winer():
+    for row in range(3):
+        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
+            return True
+    for column in range(3):
+        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            return True
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
+        return True
+    elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+        return 
+    elif empty_spaces() is False:
+        for row in range(3):
+            for column in range(3):
+                buttons[row][column].config(bg="yellow")
+        return "Draw"
+    else:
+        return False
+
+
+
+def empty_spaces():
+    spaces = 9
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]['text'] != "":
+                spaces -= 1
+    if spaces == 0:
+        return False
+    else:
+        return True
+
+
 window.mainloop()
